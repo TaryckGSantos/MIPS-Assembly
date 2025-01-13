0d784e56635dba43c6697792602fd1407646cd38
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2025-01-13 19:08:58 -0300

    Add files via upload

---
+++ Média Aritmética com Syscall/main.asm
@@ -0,0 +1,114 @@
+# Disciplina: Arquitetura e Organização de Processadores
+# Atividade: Avaliação 01 – Programação em Linguagem de Montagem
+# Exercício 01
+# Aluno: Taryck Gean Santos Pego
+
+.data
+	newline: .asciiz "\n"
+	Msg1: .asciiz "\n\n Entre com o valor de W: "
+	Msg2: .asciiz "\n\n Entre com o valor de X: "
+	Msg3: .asciiz "\n\n Entre com o valor de Y: "
+	Msg4: .asciiz "\n\n Entre com o valor de Z: "
+	Msg5: .asciiz "\n\n A média aritmética da soma é igual a: "
+	Msg6: .asciiz "\n\n Soma: "
+
+.text
+
+main:
+	# Print na mensagem 1
+	li $v0, 4 # Chama o serviço 4 (print_string)
+	la $a0, Msg1 # Msg1
+	syscall
+	
+	li $v0, 0 # Limpeza de v0
+	
+	# Lê a primeira variável
+	li $v0, 5 # Chama o serviço 5 (read_int)
+	syscall
+	
+	# Move o inteiro para o endereço t0
+	move $t0, $v0
+	
+	# Print na mensagem 2
+	li $v0, 4 # Chama o serviço 4
+	la $a0, Msg2 # Msg2
+	syscall
+	
+	li $v0, 0
+	
+	# Lê a segunda variável
+	li $v0, 5 # Chama o read int
+	syscall
+	
+	# Move o inteiro para o endereço t1
+	move $t1, $v0
+	
+	# Print na mensagem 3
+	li $v0, 4 # Chama o serviço 4
+	la $a0, Msg3 # Msg3
+	syscall
+	
+	li $v0, 0
+	
+	# Lê a terceira variável
+	li $v0, 5 # Chama o serviço 5
+	syscall
+	
+	# Move o inteiro para o endereço t2
+	move $t2, $v0
+	
+	# Print na mensagem 4
+	li $v0, 4 # Chama o serviço 4
+	la $a0, Msg4 # Msg4
+	syscall
+	
+	li $v0, 0
+	
+	# Lê a quarta variável
+	li $v0, 5 # Chama o serviço 5
+	syscall
+	
+	# Move o inteiro para o endereço t3
+	move $t3, $v0
+	
+	li $s0, 0 # limpa s0
+	
+	add $s0, $t0, $t1 # Faz a soma de $t0 e $t1 e guarda em $s0
+	add $s0, $s0, $t2 # Faz a soma de $s0 e $t2 e guarda em $s0 novamente
+	add $s0, $s0, $t3 # Faz a soma de $s0 e $t3 e guarda em $s0 novamente
+	
+	# Print na mensagem 6
+	li $v0, 4 # Chama o serviço 4
+	la $a0, Msg6 # Msg6
+	syscall
+	
+	# Print do inteiro da soma
+	li $v0, 1 # Chama o serviço 1 (print_int)
+	move $a0, $s0 # Conteúdo de $s0
+	syscall
+	
+	li $t4, 4 # Constante usada para subtração
+	li $t8, 1 # Constante usada a fim de comparação
+	li $s5, 0 # Contador da divisão
+
+	loop:
+
+		slti $t9, $s0, 4 # Caso o resultado da subtração seja menor que 4, t9 é 1
+		beq $t8, $t9, result # Caso a comparação acima seja verdadeira, pula para o resultado
+		
+		# caso nenhuma afirmação acima seja verdadeira
+		sub $s0, $s0, $t4 # subtrai 4 da soma
+		addi $s5, $s5, 1 # aumenta 1 no contador
+		j loop # Volta para o loop
+		
+	result:
+	
+		# Print na mensagem 5
+		li $v0, 4 # Chama o serviço 4 (print_string)
+		la $a0, Msg5 # Msg5
+		syscall
+		
+		# Print do inteiro $s5
+		li $v0, 1 # Chama o serviço 1 (print_int)
+		move $a0, $s5 # Conteúdo de $s5
+		syscall
---
+++ Simulação vetor/main.asm
@@ -0,0 +1,153 @@
+# Disciplina: Arquitetura e Organização de Processadores
+# Atividade: Avaliação 02 – Programação em Linguagem de Montagem
+# Exercício 01
+# Aluno: Caua Domingos e Taryck Santos
+
+.data # segmento de dados
+
+	Vetor_A: .word 0, 0, 0, 0, 0, 0, 0, 0
+	newline: .asciiz "\n"
+	Msg1: .asciiz "\n\n Entre com o tamanho do Vetor_A (máx. = 8): "
+	Msg2: .asciiz "\n Valor inválido! "
+	Msg3: .asciiz "\n\n Vetor_A["
+	Msg4: .asciiz "]: "
+	Msg5: .asciiz "\n\n Digite o índice do valor a ser impresso: " 
+	Msg6: .asciiz "\n\n O elemento do vetor na posição "
+	Msg7: .asciiz " possui o valor: "
+	
+.text # segmento de código
+
+main:
+
+	li $s1, 1 # Declarando s1 = 1
+	li $s6, 8 # Declarando s6 = 8
+
+	# Print na mensagem 1
+	li $v0, 4 # Chama o serviço 4 (print_string)
+	la $a0, Msg1 # Msg1
+	syscall
+		
+	# Lê a variável
+	li $v0, 5 # Chama o serviço 5 (read_int)
+	syscall
+		
+	move $s7, $v0 # Move o inteiro para o endereço s7
+	
+	slt $t8, $s6, $s7 # Caso o inteiro inserido seja maior que 8, t8 = 1
+	beq $t8, $s1, mensagem_invalida # Caso t8 e s1 sejam iguais, saltam para "mensagem invalida"
+	
+	li $t8, 0 # limpa t8
+	
+	slti $t8, $s7, 2 # caso s7 seja menor que 2, t8 = 1
+	beq $t8, $s1, mensagem_invalida # Caso t8 e s1 sejam iguais, saltam para "mensagem invalida"
+		
+	loop_inserir_vetor:
+			
+		# Print na mensagem 3
+		li $v0, 4 
+		la $a0, Msg3
+		syscall
+		
+		# Print na constante $t0
+		li $v0, 1 # Chama o serviço 1 (print_int)
+		move $a0, $t0 # Carrega a constante de $t0 para o syscall
+		syscall
+		
+		# Print na mensagem 4
+		li $v0, 4 
+		la $a0, Msg4
+		syscall
+		
+		# Lê a variável
+		li $v0, 5 # Chama o serviço 5 (read_int)
+		syscall
+		
+		move $t1, $v0 # Move o inteiro para o endereço t1
+		
+		sw $t1, Vetor_A($s0) # Coloca na posição $s0 do Vetor_A o valor contido em $t1
+		
+		addi $t0, $t0, 1 # adiciona 1 em t0
+		addi $s0, $s0, 4 # adiciona 4 (1 word) em s0
+		bne $t0, $s7, loop_inserir_vetor # Caso $t0 seja diferente de $s7 retorna ao loop
+	
+	inserir_indice_busca:
+		
+		li $t0, 0 # zera t0
+		li $t1, 0 # zera t1
+		li $s0, 0 # zera s0
+		
+		# Print na mensagem 5
+		li $v0, 4 
+		la $a0, Msg5 
+		syscall
+		
+		# Lê a variável
+		li $v0, 5 # Chama o serviço 5 (read_int)
+		syscall 
+		
+		move $s2, $v0 # Move o inteiro para o endereço s2
+		
+		sub $s5, $s7, $s1 # Subtrai 1 de s7 e insere em s5 (para a busca no vetor que vai de 0 até s7 - 1
+		
+		slt $t8, $s5, $s2 #  Caso o indice inserido seja maior que o numero de posições no vetor, t8 = 1
+		beq $t8, $s1, mensagem_invalida_busca # se t8 for igual a s1
+	
+		li $t8, 0
+	
+		slti $t8, $s2, 0 #  Caso o indice inserido seja menor que 0, t8 = 1
+		beq $t8, $s1, mensagem_invalida_busca # se t8 for igual a s1
+		
+		li $s0, 0 # zera s0 
+		
+		j loop_buscar_vetor
+		
+	mensagem_invalida: # função de print para ser chamada na invalidez do tamanho do vetor
+		
+		# Print na mensagem 2
+		li $v0, 4 
+		la $a0, Msg2 
+		syscall	
+		
+		j main
+		
+	mensagem_invalida_busca: # função de print para ser chamada na invalidez da busca no vetor
+		
+		# Print na mensagem 2
+		li $v0, 4 
+		la $a0, Msg2 
+		syscall	
+		
+		j inserir_indice_busca
+		
+	loop_buscar_vetor: # função de buscar no vetor 
+		
+		beq $t0, $s2, mostrar_vetor_buscado # caso a contagem de posição for igual a posição buscada, mostrar o valor naquela posição do vetor
+		
+		# caso $t0 != $s2:
+		addi $s0, $s0, 4 # adiciona 4 (1 word) em s0
+		addi $t0, $t0, 1 # adiciona 1 em t0
+		j loop_buscar_vetor # retorna ao início do loop
+		
+	mostrar_vetor_buscado: # Mostra o vetor buscado
+		
+		# Print na mensagem 6
+		li $v0, 4 
+		la $a0, Msg6 
+		syscall
+		
+		# Print na constante $t0
+		li $v0, 1
+		move $a0, $t0 
+		syscall
+		
+		# Print na mensagem 7
+		li $v0, 4 
+		la $a0, Msg7 
+		syscall
+		
+		lw $s3, Vetor_A($s0) # carrega em s3 o valor contido naquela posição do vetor 
+		
+		# Print na constante $s3
+		li $v0, 1 
+		move $a0, $s3 
+		syscall
